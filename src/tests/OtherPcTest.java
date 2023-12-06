32121bf9616f608136fc32fe5f7b2bb04295c9ee
jkdasari31 <jayakrishnardasari@gmail.com>
2023-12-06 17:43:10 +0530

    Deleted the commit from 2nd PC

--- src/tests/OtherPcTest.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class OtherPcTest {
-	public void OtherPcTestCases(String a[]) {
-		System.out.println("Committing from other PC");
-        System.out.println("Testing 1 2 3");
-	}
-}
